35b184531fe5407f2e1e5fad404d22f214a71bfe
kevinhws <kevinshu1995@gmail.com>
2024-10-19 02:47:00 +0800

    [feat] moving CUPS!

--- src/landing-page/components/CupGameCanvas.tsx
+++ src/landing-page/components/CupGameCanvas.tsx
@@ -2,9 +2,58 @@ import { useRef, useEffect } from 'react';
 import SetupDrawingCupSet from './cupGame/DrawCupSet';
 import useElementSize from '@/lib/useElementSize';
 
+type DrawEverythingArg = {
+  ctx: CanvasRenderingContext2D;
+  canvas: HTMLCanvasElement;
+};
+
 export default function CupGameCanvas() {
   const gameCanvas = useRef<HTMLCanvasElement>(null);
   const { size: canvasSize, setRef: setCanvasRef } = useElementSize();
+  const cupHeight = 120;
+  const cupTopWidth = 80;
+  const cupBottomWidth = 100;
+
+  const cupCurrentPosition = {
+    cup1: { x: 0, y: 0 },
+    cup2: { x: 0, y: 0 },
+    cup3: { x: 0, y: 0 },
+  };
+
+  function getCupPositions() {
+    const [canvasWidth, canvasHeight] = canvasSize;
+    return {
+      topLeft: {
+        x: (canvasWidth / 7) * 2 - cupBottomWidth / 2,
+        y: (canvasHeight / 5) * 2,
+      },
+      topRight: {
+        x: (canvasWidth / 7) * 5 - cupBottomWidth / 2,
+        y: (canvasHeight / 5) * 2,
+      },
+      bottom: {
+        x: (canvasWidth - cupBottomWidth) / 2,
+        y: (canvasHeight / 5) * 4,
+      },
+    };
+  }
+
+  function initCupState() {
+    const initCupsPosition = getCupPositions();
+
+    cupCurrentPosition.cup1 = {
+      x: initCupsPosition.topLeft.x,
+      y: initCupsPosition.topLeft.y,
+    };
+    cupCurrentPosition.cup2 = {
+      x: initCupsPosition.topRight.x,
+      y: initCupsPosition.topRight.y,
+    };
+    cupCurrentPosition.cup3 = {
+      x: initCupsPosition.bottom.x,
+      y: initCupsPosition.bottom.y,
+    };
+  }
 
   function updateCanvasSize({
     canvas,
@@ -20,14 +69,14 @@ export default function CupGameCanvas() {
     ctx.scale(scale, scale);
   }
 
-  function drawEverything({ ctx }: { ctx: CanvasRenderingContext2D }) {
+  function drawEverything({ ctx, canvas }: DrawEverythingArg) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.save();
     // background color
+    ctx.globalAlpha = 0.6;
     ctx.fillStyle = '#FBFBF4';
     ctx.fillRect(0, 0, canvasSize[0], canvasSize[1]);
-
-    const cupHeight = 120;
-    const cupTopWidth = 80;
-    const cupBottomWidth = 100;
+    ctx.restore();
 
     const { drawCupSet } = SetupDrawingCupSet({
       cupHeight,
@@ -36,20 +85,101 @@ export default function CupGameCanvas() {
       cupShadowHeight: cupHeight / 3,
     });
 
-    const [canvasWidth, canvasHeight] = canvasSize;
+    drawCupSet({
+      x: cupCurrentPosition.cup1.x,
+      y: cupCurrentPosition.cup1.y,
+      rotate: 0,
+      ctx,
+    });
 
-    const topLeftCupX = (canvasWidth / 7) * 2 - cupBottomWidth / 2;
-    const topLeftCupY = (canvasHeight / 5) * 2;
-    drawCupSet({ x: topLeftCupX, y: topLeftCupY, rotate: 0, ctx });
+    drawCupSet({
+      x: cupCurrentPosition.cup2.x,
+      y: cupCurrentPosition.cup2.y,
+      rotate: 0,
+      ctx,
+    });
 
-    const topRightCupX = (canvasWidth / 7) * 5 - cupBottomWidth / 2;
-    const topRightCupY = (canvasHeight / 5) * 2;
-    drawCupSet({ x: topRightCupX, y: topRightCupY, rotate: 0, ctx });
-
-    const bottomCupX = (canvasWidth - cupBottomWidth) / 2;
-    const bottomCupY = (canvasHeight / 5) * 4;
-    drawCupSet({ x: bottomCupX, y: bottomCupY, rotate: 0, ctx });
+    drawCupSet({
+      x: cupCurrentPosition.cup3.x,
+      y: cupCurrentPosition.cup3.y,
+      rotate: 0,
+      ctx,
+      hasBall: true,
+    });
   }
+
+  function moveCup(
+    cupA: 'cup1' | 'cup2' | 'cup3',
+    cupB: 'cup1' | 'cup2' | 'cup3'
+  ) {
+    return new Promise<void>(resolve => {
+      let startTime: number;
+      const cupATarget = { ...cupCurrentPosition[cupB] };
+      const cupBTarget = { ...cupCurrentPosition[cupA] };
+
+      function animate(time: number) {
+        const canvas = gameCanvas.current;
+        if (canvas === null || !canvas.getContext) return;
+        const ctx = canvas.getContext('2d');
+        if (ctx === null) return;
+
+        if (!startTime) startTime = time;
+        const timeElapsed = time - startTime;
+        const duration = 300; // 動畫持續時間，以毫秒為單位
+        const fraction = Math.min(timeElapsed / duration, 1);
+
+        // 計算新位置
+        const newCupAX =
+          cupCurrentPosition[cupA].x +
+          (cupATarget.x - cupCurrentPosition[cupA].x) * fraction;
+        const newCupAY =
+          cupCurrentPosition[cupA].y +
+          (cupATarget.y - cupCurrentPosition[cupA].y) * fraction;
+
+        const newCupBX =
+          cupCurrentPosition[cupB].x +
+          (cupBTarget.x - cupCurrentPosition[cupB].x) * fraction;
+        const newCupBY =
+          cupCurrentPosition[cupB].y +
+          (cupBTarget.y - cupCurrentPosition[cupB].y) * fraction;
+
+        cupCurrentPosition[cupA].x = newCupAX;
+        cupCurrentPosition[cupA].y = newCupAY;
+        cupCurrentPosition[cupB].x = newCupBX;
+        cupCurrentPosition[cupB].y = newCupBY;
+        drawEverything({ ctx, canvas });
+
+        if (fraction === 1) {
+          resolve();
+        } else {
+          requestAnimationFrame(animate);
+        }
+      }
+
+      requestAnimationFrame(animate);
+    });
+  }
+
+  async function moveCupSeveralTimes(times: number) {
+    type CupTag = 'cup1' | 'cup2' | 'cup3';
+    const allCups: CupTag[] = ['cup1', 'cup2', 'cup3'];
+    const randomTowCups = (cups: CupTag[]): CupTag[] => {
+      const randomIndex = Math.floor(Math.random() * cups.length);
+      const newCups = [...cups];
+      newCups.splice(randomIndex, 1);
+      return newCups;
+    };
+    const [firstCup, secondCup] = randomTowCups(allCups);
+
+    if (times === 0) return;
+
+    await moveCup(firstCup, secondCup);
+    await moveCupSeveralTimes(times - 1);
+  }
+
+  initCupState();
+
+  moveCupSeveralTimes(5);
 
   useEffect(() => {
     const canvas = gameCanvas.current;
@@ -58,16 +188,14 @@ export default function CupGameCanvas() {
     if (ctx === null) return;
 
     updateCanvasSize({ canvas, ctx });
-
-    drawEverything({ ctx });
+    drawEverything({ ctx, canvas });
   }, [gameCanvas.current, canvasSize]);
 
   return (
-    <div ref={setCanvasRef} className="w-full">
-      <canvas
-        ref={gameCanvas}
-        className="w-full h-[400px] border border-black object-contain"
-      />
+    <div>
+      <div ref={setCanvasRef} className="w-full">
+        <canvas ref={gameCanvas} className="w-full h-screen object-contain" />
+      </div>
     </div>
   );
 }
